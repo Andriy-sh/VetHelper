7ef8a19c8c5627cc10a62b8a661dbbea78d3d29b
Andriy-sh <152734833+Andriy-sh@users.noreply.github.com>
2025-04-06 02:05:00 +0300

    delete: delete empty profile page

--- src/app/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function page() {
-  return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <h1 className="text-2xl font-bold">Profile</h1>
-    </div>
-  );
-}
