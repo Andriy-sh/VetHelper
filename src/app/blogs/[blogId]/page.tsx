ee9a1b0c7c643a2f81953ab29302e3b11a2b93c9
Andriy-sh <152734833+Andriy-sh@users.noreply.github.com>
2025-04-06 00:15:51 +0300

    fix: resolve type error for dynamic route params

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,23 +1,24 @@
 import { prisma } from "../../../../prisma";
 import { auth } from "../../../../auth";
 import BlogInfo from "@/components/blogs/blog/blogInfo";
-import { ParsedUrlQuery } from "querystring";
 
 interface PageProps {
-  params: ParsedUrlQuery & { blogId: string };
+  params: { blogId: string }; // Оновлено типізацію
 }
 
 export default async function Page({ params }: PageProps) {
   const session = await auth();
   if (!session) {
-    throw new Error("aijsbdiasd");
+    throw new Error("Користувач не авторизований");
   }
+
   const user = await prisma.user.findUnique({
     where: { email: session.user?.email ?? undefined },
   });
   if (!user) {
-    throw new Error("aijsbdiasd");
+    throw new Error("Користувач не знайдений");
   }
+
   const blog = await prisma.blogPost.findUnique({
     where: { id: params.blogId },
     include: { clinic: true },
