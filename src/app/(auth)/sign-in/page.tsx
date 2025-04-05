a58486ef7160d657ada822d24e9b5f59c7b768c6
Andriy-sh <152734833+Andriy-sh@users.noreply.github.com>
2025-04-06 01:28:12 +0300

    fix: fix login

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,12 +1,6 @@
 import { SignInForm } from "@/components/auth/SignInForm";
-import { redirect } from "next/navigation";
-import { auth } from "../../../../auth";
 
 export default async function Page() {
-  const session = await auth();
-  if (session) {
-    redirect("/adding-pet");
-  }
   return (
     <div className="flex justify-center items-center h-screen w-screen">
       <SignInForm />
