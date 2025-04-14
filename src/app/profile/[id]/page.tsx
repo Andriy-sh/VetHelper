49fb15be7fd2ee7a83360ee9f6cb056ee78be3ed
Andriy-sh <152734833+Andriy-sh@users.noreply.github.com>
2025-04-15 00:58:53 +0300

    refactor(profile): optimize pet info component and fetch logic
    Move pet data fetching to the page level to reduce redundant API calls and improve performance. Enhance the PetInfo component with animations and responsive styling for better user experience.

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,6 +3,7 @@ import { auth } from "../../../../auth";
 import { prisma } from "../../../../prisma";
 import { PetInfo } from "@/components/profile/petInfo";
 import { User } from "@/lib/interface";
+import { getPets } from "@/lib/service/user";
 type Params = Promise<{ id: string }>;
 export default async function page({ params }: { params: Params }) {
   const resolvedParams = await params;
@@ -13,6 +14,7 @@ export default async function page({ params }: { params: Params }) {
   if (!session?.user?.email) {
     throw new Error("User not authenticated");
   }
+  const pets = await getPets(user?.id || "");
 
   const appointment = await prisma.appointment.findMany({
     where: {
@@ -32,8 +34,13 @@ export default async function page({ params }: { params: Params }) {
         clinics={clinics}
       />
 
-      <div className="w-[400px] rounded-2xl  max-h-[72vh]  bg-white  flex  flex-col justify-top">
-        <PetInfo userId={user?.id || ""} />
+      <div className="w-[350px] rounded-2xl  max-h-[72vh]  bg-white  flex  flex-col justify-top">
+        <PetInfo
+          userId={user?.id || ""}
+          pets={pets}
+          session={session}
+          user={user || ({} as User)}
+        />
       </div>
     </div>
   );
--- src/components/profile/petInfo.tsx
+++ src/components/profile/petInfo.tsx
@@ -1,35 +1,58 @@
-// app/profile/[id]/page.js
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { FaPaw } from "react-icons/fa";
 import { PlusCircle } from "lucide-react";
-import { getPets, getUser } from "@/lib/service/user";
-import { auth } from "../../../auth";
+import { Pet, User } from "@/lib/interface";
+import { Session } from "next-auth";
+import { motion } from "framer-motion";
 
-export const PetInfo = async ({ userId }: { userId: string }) => {
-  const session = await auth();
-
+export const PetInfo = ({
+  userId,
+  pets,
+  user,
+  session,
+}: {
+  userId: string;
+  pets: Pet[];
+  user: User;
+  session: Session;
+}) => {
   if (!session?.user?.email) {
     throw new Error("User not authenticated");
   }
-
-  const user = await getUser({ user: { email: session.user.email } });
-  const pets = await getPets(userId);
 
   const profileId = userId;
   const isOwner = user.id === profileId;
 
   return (
-    <div className="space-y-4 py-12">
-      <h2 className="text-3xl font-bold text-center">
-        Улюбленці ({pets.length})
-      </h2>
+    <div className="space-y-4 ">
+      <div className="flex justify-center items-center">
+        <h2 className="text-3xl font-bold text-center flex space-x-4">
+          Улюбленці
+          <span className=" ml-2 flex justify-center items-center text-3xl font-bold w-10 h-10 bg-red-400 rounded-full">
+            <span>{pets.length}</span>
+          </span>
+        </h2>
+      </div>
+
       <hr className="my-2" />
-      <div className="flex flex-col items-center justify-center overflow-auto max-h-[64vh]">
-        <div className="max-w-[110vh] mt-5 space-y-4">
+      <div className="flex flex-col justify-center overflow-auto ">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className=" mt-5 space-y-4 max-h-[64vh] overflow-x-hidden "
+        >
           {pets.length > 0 ? (
-            pets.map((pet) => (
-              <div
+            pets.map((pet, inx) => (
+              <motion.div
+                initial={{ x: 300 }}
+                animate={{ x: 0 }}
+                transition={{
+                  duration: 0.5,
+                  delay: 0.1 * inx,
+                }}
                 key={pet.id}
                 className="flex items-center gap-4 p-4 bg-white rounded-lg shadow-md hover:shadow-xl transition"
               >
@@ -59,25 +82,30 @@ export const PetInfo = async ({ userId }: { userId: string }) => {
                     <p className="text-gray-600">Порода: {pet.breed}</p>
                   </div>
                 </Link>
-              </div>
+              </motion.div>
             ))
           ) : (
             <p className="text-gray-600 text-center py-8">
               Тваринки не знайдені
             </p>
           )}
-        </div>
-        {isOwner && (
-          <div className="flex justify-center pt-4">
-            <Link
-              href="/adding-pet"
-              className="flex items-center gap-2 px-6 py-2 bg-blue-600 text-white rounded-full shadow-md hover:bg-blue-700 transition"
-            >
-              <PlusCircle size={20} /> Додати улюбленця
-            </Link>
-          </div>
-        )}
+        </motion.div>
       </div>
+      {isOwner && (
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2 }}
+          className="flex justify-center"
+        >
+          <Link
+            href="/adding-pet"
+            className="flex items-center gap-2 px-6 py-2 bg-blue-600 text-white rounded-full shadow-md hover:bg-blue-700 transition"
+          >
+            <PlusCircle size={20} /> Додати улюбленця
+          </Link>
+        </motion.div>
+      )}
     </div>
   );
 };
